2b3f61b7571ba9bc5d0a196645544f421f3f65ca
Ramprasad <ramtrg@gmail.com>
2020-10-18 13:41:06 +0530

    initial commit

---
+++ app/src/main/java/com/rramprasad/gitsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rramprasad.gitsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
